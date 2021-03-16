a7f93d69a57c30e3a776fffc8a5964b296b6cd71
Hohjanna <hohjanna@MacBook-Pro-de-Hohjanna.local>
2021-03-16 08:40:14 -0500

    request API con fetch

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/lol/lol.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);//
+
+
+
+
+//obtener data con Fetch (API resquest)
+
+const apiUrl = './data/lol/lol.js';
+
+async function getData() {
+    const response = await fetch(apiUrl);
+    const data = await response.text();
+    console.log(data);
+}
+
+getData();
+
+
+//Recorrer cada objeto de la data y pintarlo en el html
