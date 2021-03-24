e49078d1b97b22bd961b83f0277b3136034a4d0c
Hohjanna <hohjanna@MacBook-Pro-de-Hohjanna.local>
2021-03-24 16:32:32 -0500

    Filtro barra de busqueda

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import { example } from './data.js';
 import data from './data/lol/lol.js';
-let lol = Object.values(data.data); //se convierte el objeto a un array
+const lol = Object.values(data.data); //se convierte el objeto a un array
 //console.log("arrayDatos", typeof(lol));
-
-
-
+//Aquí va la manipulación del DOM
 
 document.getElementById("pantalla-1").style = "display:normal"
 document.getElementById("pantalla-2").style = "display:none" //Esconder la segunda pantalla
-
 
 const btnChooseRole = document.getElementById("btnData"); //Función botón elegir campeon
 btnChooseRole.addEventListener('click', btnData);
@@ -24,12 +21,17 @@ function btnData() {
 
 function showRoles(data) {
     const mainContainer = document.getElementById("dataLol");
+    if (mainContainer.hasChildNodes()) {
+        while (mainContainer.childNodes.length >= 1) {
+            mainContainer.removeChild(mainContainer.firstChild);
+        }
+    }
     for (let i = 0; i < data.length; i++) {
         let div = document.createElement("div");
         div.innerHTML = /*'Name: ' + data[i].name + data[i].title;*/ (`
         
         <div id="card-body">
-            <div class="card-image" style="width: 20em;">
+            <div class="card-image" style="width: 16em;">
             <img src="${data[i].splash}" id="img" alt="splashChampion">
             <h2 class="card-title">${data[i].name}</h2>
             </div>
@@ -42,6 +44,81 @@ function showRoles(data) {
 }
 
 showRoles(lol);
+
+
+
+
+
+
+
+// Barra de búsqueda (filtro)
+const barraBusqueda = document.getElementById('barraBusqueda');
+
+//let arrkeys = lol;
+//console.log(arrkeys);
+
+//arrkeys = [];
+barraBusqueda.addEventListener('keyup', (e) => {
+    const inputString = e.target.value.toLowerCase();
+    if (inputString === "") {
+        return showRoles(lol);
+    }
+    const filtroCampeones = lol.filter((campeones) => {
+        //console.log(`${campeones.name}`);
+        //console.log(`${campeones.name}`.toLowerCase().includes(inputString));
+        return `${campeones.name}`.toLowerCase().includes(inputString);
+    });
+    showRoles(filtroCampeones);
+});
+
+function cleanInput() {
+    document.getElementById('barraBusqueda').reset();
+}
+
+cleanInput(filtroCampeones);
+
+
+
+
+
+/*function myFunction() {
+    const input = document.getElementById('barraBusqueda');
+    const filter = input.value.toUpperCase();
+    const getData = arrkeys;
+    for (i = 0; i < arrkeys.length; i++) {
+        a = arrkeys[i].name;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            arrkeys[i].style.display = "";
+        } else {
+            arrkeys[i].style.display = "none";
+        }
+    }
+}
+
+myFunction(lol );*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
